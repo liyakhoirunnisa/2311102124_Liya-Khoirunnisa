bacebc43537ccdb70ec83de4ca75346dcdd6e11c
liyakhoirunnisa <lykhoirunnisa@gmail.com>
2024-03-09 18:10:09 +0700

    tambah komentar

--- pertemuan01/guided2.cpp
+++ pertemuan01/guided2.cpp
@@ -1,21 +1,29 @@
 #include <stdio.h>
 #include <string.h>
 
+//Struct
 struct Mahasiswa
 {
     char name[50];
     char address[100];
     int age;
 };
-int main()
-{
+
+//Tipe data abstrak Liya
+
+int main() {
+    //Menggunakan struct
     struct Mahasiswa mhs1, mhs2;
+
+    //Mengisi nilai ke struct
     strcpy(mhs1.name, "Dian");
     strcpy(mhs1.address, "Mataram");
     mhs1.age = 22;
     strcpy(mhs2.name, "Bambang");
     strcpy(mhs2.address, "Surabaya");
     mhs2.age = 23;
+
+    //Mencetak isi struct
     printf("## Mahasiswa 1 ##\n");
     printf("Nama: %s\n", mhs1.name);
     printf("Alamat: %s\n", mhs1.address);
@@ -25,5 +33,6 @@ int main()
     printf("Nama: %s\n", mhs2.name);
     printf("Alamat: %s\n", mhs2.address);
     printf("Umur: %d\n", mhs2.age);
+
     return 0;
 }
